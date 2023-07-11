44386259ac358536d5e3bbbea4362f3b50b53d95
Sujeet Kumar Rath <93946478+skr18@users.noreply.github.com>
2023-07-11 11:41:58 +0530

    fixed bugs

--- script.js
+++ script.js
@@ -12,18 +12,15 @@ var presentState = document.getElementById("pstate");
 var presentCity = document.getElementById("pcity");
 var presentPostal = document.getElementById("p-postal");
 
-
-
-var copyCheckbox = document.getElementById("copydataCheckbox");
-copyCheckbox.addEventListener("click",async()=>{
-
-    // let checkbox = document.getElementById("copydataCheckbox")
-    // if(checkbox.checked == true){
-    //     checkbox.checked = false;
-    // }else{
-    //     checkbox.checked = true
-    // }
+copyDataDiv.addEventListener("click",async()=>{
     
+    var copyCheckbox = document.getElementById("copydataCheckbox");
+    if(copyCheckbox.checked == true){
+        copyCheckbox.checked = false;
+    }else{
+            copyCheckbox.checked = true
+    }
+            
     let flag = 0
     for(let i=5;i<=9;i++){
         if(errormsg[i].status==true){
@@ -37,10 +34,8 @@ copyCheckbox.addEventListener("click",async()=>{
                 errormsg[i].style.visibility="visible"
             }
         } 
-        console.log("heyyy")
     }
     else if(copyCheckbox.checked==true && flag==0){
-          
         presentAddressLine1.value = permanentAddressLine1.value
         presentAddressLine2.value = permanentAddressLine2.value
         presentPostal.value = permanentPostal.value
@@ -58,7 +53,6 @@ copyCheckbox.addEventListener("click",async()=>{
         }
     }
     else{
-        console.log("elsle")
         presentAddressLine1.value="";
         presentPostal.value=""
 
@@ -75,7 +69,7 @@ copyCheckbox.addEventListener("click",async()=>{
         option2.disabled=true
         option2.hidden=true
 
-        option1.text = "Tap To Select State";
+        option2.text = "Tap To Select State";
         presentState.add(option2)
 
         let option3 = document.createElement("option")
@@ -83,7 +77,7 @@ copyCheckbox.addEventListener("click",async()=>{
         option3.disabled=true
         option3.hidden=true
 
-        option1.text = "Tap To Select Cities";
+        option3.text = "Tap To Select Cities";
         presentCity.add(option3)
 
     }
@@ -95,16 +89,13 @@ var allcountry ,allstate , allcities
 async function initiate(){
 
     var req = await fetch("https://www.universal-tutorial.com/api/getaccesstoken",{
-    
     headers:{
         "Accept": "application/json",
         "api-token": "QXbOD1ZwyZV2sxx8mcbeKOhKAe5ve9m1I8mJIoJ1O4UCHSNHWamKU2QjZNT0mFpt5bQ",
         "user-email": "sujeetrath48@gmail.com"
     }});
     
-    
     authtoken = await req.json();
-
     const responce = await fetch("https://www.universal-tutorial.com/api/countries",{
         headers:{
             "Authorization":`Bearer ${authtoken.auth_token}`,
@@ -122,12 +113,10 @@ initiate();
 async function fetchcountry(){
 
     permanentCountry.innerHTML = ""
-
     let opt = document.createElement("option")   
     opt.selected=true
     opt.disabled=true
-    opt.hidden=true
-    
+    opt.hidden=true 
     opt.text = "Tap To Select Country";
     permanentCountry.add(opt)
 
@@ -146,12 +135,10 @@ async function fetchcountry(){
 async function pfetchcountry(){
 
         presentCountry.innerHTML = ""
-
         let opt = document.createElement("option")   
         opt.selected=true
         opt.disabled=true
         opt.hidden=true
-        
         opt.text = "Tap To Select Country";
         presentCountry.add(opt)
 
@@ -175,7 +162,6 @@ async function fetchstate(){
     opt.selected=true
     opt.disabled=true
     opt.hidden=true
-    
     opt.text = "Tap To Select state";
     permanentState.add(opt)
 
@@ -187,16 +173,11 @@ async function fetchstate(){
         }
     })
 
-    allstate = await responce.json();
-    
-    for(let state of allstate){
-        
-        var opt1 = document.createElement("option")
-        
+    allstate = await responce.json();  
+    for(let state of allstate){     
+        var opt1 = document.createElement("option") 
         opt1.value = state.state_name;
-        
         opt1.text = state.state_name;
-
         permanentState.add(opt1)
     }
 
@@ -212,8 +193,7 @@ async function pfetchstate(){
     let opt = document.createElement("option")   
     opt.selected=true
     opt.disabled=true
-    opt.hidden=true
-    
+    opt.hidden=true 
     opt.text = "Tap To Select state";
     presentState.add(opt)
 
@@ -225,16 +205,11 @@ async function pfetchstate(){
         }
     })
 
-    allstate = await responce.json();
-    
-    for(let st of allstate){
-        
+    allstate = await responce.json();   
+    for(let st of allstate){  
         var opt1 = document.createElement("option")
-        
         opt1.value = st.state_name;
-        
         opt1.text = st.state_name;
-
         presentState.add(opt1)
     }
 
@@ -251,8 +226,7 @@ async function fetchcity(){
     let opt = document.createElement("option")   
     opt.selected=true
     opt.disabled=true
-    opt.hidden=true
-    
+    opt.hidden=true   
     opt.text = "Tap To Select city";
     permanentCity.add(opt)
 
@@ -263,28 +237,22 @@ async function fetchcity(){
             "Accept": "application/json"
         }
     })
+
     allcities = await responce.json();
-    
     for(let cities of allcities){
-        
         var opt1 = document.createElement("option")
-        
-        opt1.value = cities.city_name;
-        
+        opt1.value = cities.city_name; 
         opt1.text = cities.city_name;
         permanentCity.add(opt1)
     }
-
 }
-
 async function pfetchcity(){
 
     presentCity.innerHTML = ""
     let opt = document.createElement("option")   
     opt.selected=true
     opt.disabled=true
-    opt.hidden=true
-    
+    opt.hidden=true  
     opt.text = "Tap To Select city";
     presentCity.add(opt)
 
@@ -298,26 +266,14 @@ async function pfetchcity(){
     allcities = await responce.json();
     
     for(let cities of allcities){
-        
         var opt1 = document.createElement("option")
-        
-        opt1.value = cities.city_name;
-        
+        opt1.value = cities.city_name; 
         opt1.text = cities.city_name;
         presentCity.add(opt1)
     }
-
 }
-
-
-// API -- QXbOD1ZwyZV2sxx8mcbeKOhKAe5ve9m1I8mJIoJ1O4UCHSNHWamKU2QjZNT0mFpt5bQ
-
 //---------------------------------------Validation Start -----------------------------------------------------
-
-
-
 //user profile validation
-
 //user photo
 let cons = document.getElementById("profileimg")
 cons.addEventListener("change",function (e){
@@ -346,55 +302,34 @@ errormsg[3].status=true;
 errormsg[4]=document.getElementById("spanGender")
 errormsg[4].status=true;
 
-firstname.addEventListener('input',(e)=>{
+function checkforValue(e,id){
     if(e.target.value==""){
-        errormsg[0].style.visibility="visible"
-        errormsg[0].status=true;
+        errormsg[id].style.visibility="visible"
+        errormsg[id].status=true;
     }else{
-        errormsg[0].style.visibility="hidden"
-        errormsg[0].status=false;
+        errormsg[id].style.visibility="hidden"
+        errormsg[id].status=false;
     }
+}
+
+firstname.addEventListener('input',(e)=>{
+   checkforValue(e,0);
 })
 
-
 lastname.addEventListener('input',(e)=>{
-    if(e.target.value==""){
-        errormsg[1].style.visibility="visible"
-        errormsg[1].status=true;
-    }else{
-        errormsg[1].style.visibility="hidden"
-        errormsg[1].status=false;
-    }
+    checkforValue(e,1);
 })
 
 email.addEventListener('input',(e)=>{
-    if(e.target.value==""){
-        errormsg[2].style.visibility="visible"
-        errormsg[2].status=true;
-    }else{
-        errormsg[2].style.visibility="hidden"
-        errormsg[2].status=false;
-    }
+    checkforValue(e,2);
 })
 
 dob.addEventListener('input',(e)=>{
-    if(e.target.value==""){
-        errormsg[3].style.visibility="visible"
-        errormsg[3].status=true;
-    }else{
-        errormsg[3].style.visibility="hidden"
-        errormsg[3].status=false;
-    }
+    checkforValue(e,3);
 })
 
 gender.addEventListener('change',(e)=>{
-    if(e.target.value==""){
-        errormsg[4].style.visibility="visible"
-        errormsg[4].status=true;
-    }else{
-        errormsg[4].style.visibility="hidden"
-        errormsg[4].status=false;
-    }
+    checkforValue(e,4);
 })
 
 //permanent address validation
@@ -415,55 +350,24 @@ errormsg[9]=document.getElementById("spancity")
 errormsg[9].status=true;
 
 permanentAddressLine1.addEventListener('input',(e)=>{
-    if(e.target.value==""){
-        errormsg[5].style.visibility="visible"
-        errormsg[5].status=true;
-    }else{
-        errormsg[5].style.visibility="hidden"
-        errormsg[5].status=false;
-    }
+    checkforValue(e,5);
 })
 
 permanentPostal.addEventListener('input',(e)=>{
-    if(e.target.value==""){
-        errormsg[6].style.visibility="visible"
-        errormsg[6].status=true;
-    }else{
-        errormsg[6].style.visibility="hidden"
-        errormsg[6].status=false;
-    }
+    checkforValue(e,6);
 })
 
 permanentCountry.addEventListener('input',(e)=>{
-    if(e.target.value==""){
-        errormsg[7].style.visibility="visible"
-        errormsg[7].status=true;
-    }else{
-        errormsg[7].style.visibility="hidden"
-        errormsg[7].status=false;
-    }
+    checkforValue(e,7);
 })
 
 permanentState.addEventListener('input',(e)=>{
-    if(e.target.value==""){
-        errormsg[8].style.visibility="visible"
-        errormsg[8].status=true;
-    }else{
-        errormsg[8].style.visibility="hidden"
-        errormsg[8].status=false;
-    }
+    checkforValue(e,8);
 })
 
 permanentCity.addEventListener('input',(e)=>{
-    if(e.target.value==""){
-        errormsg[9].style.visibility="visible"
-        errormsg[9].status=true;
-    }else{
-        errormsg[9].style.visibility="hidden"
-        errormsg[9].status=false;
-    }
+    checkforValue(e,9);
 })
-
 
 //persent address validation
 
@@ -483,61 +387,26 @@ errormsg[14]=document.getElementById("spanp-city")
 errormsg[14].status=true;
 
 presentAddressLine1.addEventListener('input',(e)=>{
-    if(e.target.value==""){
-        errormsg[10].style.visibility="visible"
-        errormsg[10].status=true;
-    }else{
-        errormsg[10].style.visibility="hidden"
-        errormsg[10].status=false;
-    }
+    checkforValue(e,10);
 })
 
 presentPostal.addEventListener('input',(e)=>{
-    if(e.target.value==""){
-        errormsg[11].style.visibility="visible"
-        errormsg[11].status=true;
-    }else{
-        errormsg[11].style.visibility="hidden"
-        errormsg[11].status=false;
-    }
+    checkforValue(e,11);
 })
 
 presentCountry.addEventListener('input',(e)=>{
-    if(e.target.value=="" && country.value==""){
-        errormsg[12].style.visibility="visible"
-        errormsg[12].status=true;
-    }else{
-        errormsg[12].style.visibility="hidden"
-        errormsg[12].status=false;
-    }
+    checkforValue(e,12);
 })
 
 presentState.addEventListener('input',(e)=>{
-
-    if(e.target.value=="" && state.value==""){
-        errormsg[13].style.visibility="visible"
-        errormsg[13].status=true;
-    }else{
-        errormsg[13].style.visibility="hidden"
-        errormsg[13].status=false;
-    }
+    checkforValue(e,13);
 })
 
 presentCity.addEventListener('input',(e)=>{
-
-    if(e.target.value=="" && state.value==""){
-        errormsg[14].style.visibility="visible"
-        errormsg[14].status=true;
-    }else{
-        errormsg[14].style.visibility="hidden"
-        errormsg[14].status=false;
-    }
+    checkforValue(e,14);
 })
 
 submitButton.addEventListener('click',(e)=>{
-
-
-
     e.preventDefault();
 
     var inputEmail =document.getElementById("emailinp").value  
@@ -560,69 +429,40 @@ submitButton.addEventListener('click',(e)=>{
             }
         }
     }
-    
-    
+
     if(flag==false){
         document.getElementsByClassName("userContainer")[0].style.display="block"
-
-        
+            
         var hobbbyvalue=""
-    
-        if(hobby1.checked==true){
-            hobbbyvalue += hobby1.value + ", "
+        var allhobbies = document.getElementById("hobbiesDiv").getElementsByTagName("label")
+        for(tags of allhobbies){
+            let hobbycheckbox = document.getElementById(tags.id).getElementsByTagName("input")
+            if(hobbycheckbox[0].checked){
+                hobbbyvalue+=hobbycheckbox[0].value +", "
+            }
         }
-        if(hobby2.checked==true){
-            hobbbyvalue += hobby2.value + ", "
-        }
-        if(hobby3.checked==true){
-            hobbbyvalue += hobby3.value + ", "
-        }
-        if(hobby4.checked==true){
-            hobbbyvalue += hobby4.value
-        }
+        userHobbies.innerHTML = hobbbyvalue
     
         var allinputtags = document.getElementById("userProfile").getElementsByTagName("input")
         for(tags of allinputtags){
             if(tags.attributes.data_id){
-    
                 document.getElementById(tags.attributes.data_id.value).innerHTML = tags.value
             }
         }
-    
         var allselecttags = document.getElementById("userProfile").getElementsByTagName("select")
         for(tags of allselecttags){
             if(tags.attributes.data_id){
                 document.getElementById(tags.attributes.data_id.value).innerHTML = tags.value
             }
+            if(tags.value=""){
+                document.getElementById(tags.attributes.data_id.value).innerHTML = "Na"
+            }
         }
-
-        if(permanentAddressLine2.value==""){
-            userPresentAddress2.innerHTML = "NA"
-        }else{
-            userPresentAddress2.innerHTML = permanentAddressLine2.value
-        }
-
-        if(presentAddressLine2.value==""){
-            userPermanentAddress2.innerHTML = "NA"
-        }else{
-            userPermanentAddress2.innerHTML = presentAddressLine2.value
-        }
-        userHobbies.innerHTML = hobbbyvalue
         if(checkbox.checked==true){
             userSubcription.innerHTML="Yes"
         }else{
             userSubcription.innerHTML="No"
         }
-    }
-})
-
-var checkbox = document.getElementById("subcriptionCheckBox")
-
-checkbox.addEventListener("click",()=>{
-    if(checkbox.checked == true){
-        checkbox.checked = false;
-    }else{
-        checkbox.checked = true
     }
 })
 
@@ -636,7 +476,3 @@ hobby.addEventListener("click",(e)=>{
         hobbyButton.style.display="flex"
     }
 })
-
-
-
-
