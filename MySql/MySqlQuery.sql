9aa192658f731c8b1da471f6112c76e092b28a2a
Sujeet Kumar Rath <93946478+skr18@users.noreply.github.com>
2023-07-14 17:44:43 +0530

    Added stored procedure & Views

--- MySql/MySqlQuery.sql
+++ MySql/MySqlQuery.sql
@@ -68,4 +68,40 @@ SELECT * from UserData;
 truncate table UserData;
 
 
+-- procedure  - -- - -  -- - - -- - - - - 
+
+
+DELIMITER $$
+USE `profileapplication`$$
+CREATE PROCEDURE `getRoleName` (name varchar(20))
+BEGIN
+	SELECT * FROM userrolestatic WHERE roleID = 
+    (select roleId from userrole where userId = 
+    (select userId from userdata where firstname=name)) ;
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+USE `profileapplication`$$
+CREATE PROCEDURE `getCountryName` (id int)
+BEGIN
+	SELECT * FROM country WHERE countryID = 
+    (select countryId from userdata where countryId=id) ;
+END$$
+
+DELIMITER ;
+
+call getUserDetails(81);
+
+call getRoleName("sujeet");
+
+call getCountryName(91);
+
+
+-- views -----------------
+
+
+select * from state_view;
+
 
